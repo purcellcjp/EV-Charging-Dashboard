081eb02c059c4152f2e726e600cbe53b56915817
Dawson Martin-Jones <martinjonesdawson@gmail.com>
2024-10-15 22:20:17 -0400

    update

--- Website_Creation/logic.js
+++ Website_Creation/logic.js
@@ -1,32 +1,41 @@
-fetch('NREL_GeoJSON.geojson')
-  .then(response => response.json())
-  .then(data => {
-    // Create the map
-    let myMap = L.map("map", {
-      center: [38.37, -77.01],
-      zoom: 7.5
-    });
+document.addEventListener("DOMContentLoaded", function () {
+//fetch the GeoJSON data
+  fetch('NREL_GeoJSON.geojson')
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+//create the map
+      let myMap = L.map("map", {
+        center: [39.155907, -76.717216],
+        zoom: 7.5
+      });
 
-    // Add the street layer
-    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(myMap);
+      // Add the street layer
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(myMap);
 
-    // Process the GeoJSON data and add markers
-    L.geoJSON(data, {
-      onEachFeature: function (feature, layer) {
-        // If the feature has properties, bind a popup
-        if (feature.properties) {
-          let popupContent = `<strong>${feature.properties['Station Name']}</strong><br>
-                              ID: ${feature.properties['ID']}<br>
-                              Address: ${feature.properties['Address']}, ${feature.properties['City']}, ${feature.properties['State']} ${feature.properties['Zipcode']}<br>
-                              Access: ${feature.properties['Access']}<br>
-                              Owner: ${feature.properties['Owner']}<br>
-                              Last Used: ${feature.properties['Last_Used']}<br>
-                              Open Date: ${feature.properties['Open_Date']}<br>
-                              Connection Type: ${feature.properties['Connection_Type']}`;
-          layer.bindPopup(popupContent);
+//process the GeoJSON data and add markers
+      L.geoJSON(data, {
+        onEachFeature: function (feature, layer) {
+          if (feature.properties) {
+            let popupContent = `<strong>${feature.properties['Station Name']}</strong><br>
+                                ID: ${feature.properties['ID']}<br>
+                                Address: ${feature.properties['Address']}, ${feature.properties['City']}, ${feature.properties['State']} ${feature.properties['Zipcode']}<br>
+                                Access: ${feature.properties['Access']}<br>
+                                Owner: ${feature.properties['Owner']}<br>
+                                Last Used: ${feature.properties['Last_Used']}<br>
+                                Open Date: ${feature.properties['Open_Date']}<br>
+                                Connection Type: ${feature.properties['Connection_Type']}`;
+            layer.bindPopup(popupContent);
+          }
         }
-      }
-    }).addTo(myMap);
-  });
+      }).addTo(myMap);
+
+//click to open new HTML
+      myMap.on('click', function() {
+        window.open('map.html', '_blank'); 
+      });
+    });
+});
